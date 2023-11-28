1acf75bda6f8dbdf746a11cb6c4341ba92cca518
Jordan Ewings <jordanewings@outlook.com>
2023-11-28 12:28:30 -0600

    gen

--- scripts/procGamesResponses.js
+++ scripts/procGamesResponses.js
@@ -3,13 +3,14 @@
 function procGamesResponses(dbGames, dbResponses) {
   let data = dbGames.map((game) => {
     let responses = dbResponses.filter((x) => x['game_id'] == game['game_id']);
+    game.datetime = game.gametime;
     game.gameday = game.gametime_short.split(' @ ')[0];
     game.gameday_long = game.gametime_long.split(' @ ')[0];
     game.gametime = game.gametime_short.split(' @ ')[1];
     game.away_logo = (game.away_team != 'TBD') ? game.away_logo.replace('500', '500-dark') : game.away_logo;
     game.home_logo = (game.home_team != 'TBD') ? game.home_logo.replace('500', '500-dark') : game.home_logo;
     let rec = [
-      'game_id', 'week', 'week_label', 'gameday_long', 'gameday', 'gametime', 'state', 'stateshort',
+      'game_id', 'datetime', 'week', 'week_label', 'gameday_long', 'gameday', 'gametime', 'state', 'stateshort',
       'away_team', 'away_teamshort', 'away_teamfull', 'away_record', 'away_logo', 'away_score', 'away_color',
       'home_team', 'home_teamshort', 'home_teamfull', 'home_record', 'home_logo', 'home_score', 'home_color',
       'spread', 'win_team']
